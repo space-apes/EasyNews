2934e04f4457e7651f15406e4e3d8cbd7f75e5b2
Andrew A <33008952+andrewronald@users.noreply.github.com>
2019-11-12 18:47:29 -0800

    Update admin.py

--- news/agentportal/admin.py
+++ news/agentportal/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from agentportal.models import Customer, Agent
+def CustomerAdmin(admin.ModelAdmin):
+  list_display = ['name','address']
+  ordering = ['name']
+  
+ admin.site.register(Article, ArticleAdmin)
 # Register your models here.
