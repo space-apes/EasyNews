3aa285ee179190b9abd0d4e8a6075a932cca2357
Andrew A <33008952+andrewronald@users.noreply.github.com>
2019-11-12 19:35:00 -0800

    Update urls.py

--- news/agentportal/urls.py
+++ news/agentportal/urls.py
@@ -6,6 +6,5 @@ app_name = 'agentportal'
 urlpatterns = [
         path('', views.index, name='index'),
 	path('showDeliveries', views.showDeliveries, name='showDeliveries'),
-	path('accounts/', include('django.contrib.auth.urls')),
 ]
 
