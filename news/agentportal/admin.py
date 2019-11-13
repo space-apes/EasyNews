e6d09e6585fc86430e6d0a8827a53f16fad2c7b3
Andrew A <33008952+andrewronald@users.noreply.github.com>
2019-11-12 19:07:48 -0800

    Update admin.py
    added set agent actions

--- news/agentportal/admin.py
+++ news/agentportal/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from agentportal.models import Customer, Agent
-def CustomerAdmin(admin.ModelAdmin):
+def set_agent1(modeladmin, request, queryset):
+  queryset.update(agent=Agent.objects.get(pk=1))
+  set_agent1.short_description="set these agents to agent 1"
+  
+class CustomerAdmin(admin.ModelAdmin):
   list_display = ['name','address']
   ordering = ['name']
+  actions=[set_agent1]
   
- admin.site.register(Article, ArticleAdmin)
+ admin.site.register(Customer, CustomerAdmin)
 # Register your models here.
