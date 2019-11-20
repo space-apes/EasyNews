e6f62f207d00dd2791e9ed3f471ecd9208032f43
Brian Smith <beesmiththrowaway@gmail.com>
2019-11-19 18:20:11 -0800

    fixed local timezone problem that was causing deliveries to not show

--- news/agentportal/views.py
+++ news/agentportal/views.py
@@ -8,9 +8,9 @@ from django.contrib.auth.decorators import login_required
 from agentportal import config
 from django import template
 from django.http import HttpResponse
+from django.utils import timezone
 register = template.Library()
 # Create your views here.
-
 
 
 #helper function to populate list of deliveries for today
@@ -48,11 +48,11 @@ def logoutPage(request):
 	return redirect('/agentportal/')
 
 
-#check if authenticated
-#	if not, send to logout view
-#
-#
-#
+##MAIN LOGIC FOR DELIVERY VIEWING
+#	contains options for single day or range of dates (mode=day vs mode = range)
+#	when transitioning from login screen, always set mode to day
+#	when submitting form from this page, reloads the same page with mode=range, using values for beginning and end date from form 
+#	
 def showDeliveries(request, mode): 
 #	if not authenticated, return to login
 	if not request.user.is_authenticated:
@@ -60,23 +60,28 @@ def showDeliveries(request, mode):
 		
 #	list of delivery info that will be passed to template to display deliveries
 	dateNameAddressList=[]
-
+	todaysDate = timezone.localtime(timezone.now()).date()
 #	set mode to either deliver just today's deliveries or between a range. 		
 	if mode=="day":
-		begDate = date.today()
-		endDate = date.today()
+		begDate = todaysDate
+		endDate = todaysDate
 	else:
 		begDate = request.POST['startDateForm']
 		endDate = request.POST['endDateForm']
-		print("you chose to see deliveries in range mode! post date for begDate is %s and endDate is %s" % (begDate, endDate))
-		print("whereas the format for date.today is %s" % (date.today()))
-	
+#		if someone submits empty date, set date to current date
+		if not begDate:
+			begdate = todaysDate
+		if not endDate:
+			endDate = todaysDate
+
 #	populate list nameAddressList with {date,name, address} dicts to fill the 	
 	for delivery in Delivery.objects.filter(user=request.user).filter(date__gte=begDate).filter(date__lte=endDate):
 		dateNameAddressList.append({'date': delivery.date, 'name':delivery.customer.name, 'address':delivery.customer.address})
 	
-#	return render(request, 'agentportal/index.html', {'agentName':agentName, 'dateNameAddressList':dateNameAddressList})
-	return render(request, 'agentportal/index.html', {'userName':request.user.username, 'dateNameAddressList':dateNameAddressList, 'mapURL':generateMap(request)})
+	tailored_url = config.base_url+'&size=320x320'+'&maptype=roadmap&markers=color:blue|'+"".join([dateNameAddress['address'] for dateNameAddress in dateNameAddressList])+'&key='+config.api_key
+	print('tailored_url is: %s' % tailored_url)
+	return render(request, 'agentportal/index.html', {'userName':request.user.username, 'dateNameAddressList':dateNameAddressList, 'mapURL':tailored_url, 'todaysDate': todaysDate})
+
 def generateMap(request):
 	listOfAddresses = []
 	for delivery in Delivery.objects.filter(user=request.user):
