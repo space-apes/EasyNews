2fbfccbd28dd7eb1643b2b480cbfc37afbe61119
Brian Smith <beesmiththrowaway@gmail.com>
2019-12-02 10:54:40 -0800

    fixed bill calculation: 	-calculates all months and years from begDate to endDate 	-calculates from 21st of last month to 20th of bill's month
    added nice comments to explain everything in views

--- news/agentportal/views.py
+++ news/agentportal/views.py
@@ -1,3 +1,16 @@
+#***EASYNEWS NEWSPAPER DELIVERY SYSTEM VIEWS****
+#
+#Andrew Aguilar
+#Brian Smith
+#Courtney Castrey
+#Hilario Balajadia Jr. 
+#Matthew Siewierski
+#Shinaola Agbede
+#
+#CSC-581 Software Engineering
+#Fall 2019
+#California State University Dominguez Hills
+
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Customer, Delivery
@@ -9,19 +22,86 @@ from agentportal import config
 from django import template
 from django.http import HttpResponse
 from django.utils import timezone
+from datetime import date
 register = template.Library()
-# Create your views here.
+
+#****************VIEWS**********************: 
+#these functions perform all server side computation and ultimately direct user's browser to new page.
+#After a user goes to a particular url, they will call one of these view functions. The mapping of URL->view function is contained in
+#agentportal.urls.py and /urls.py files.
+#The view functions can: 	
+#
+#	-take POSTed user input from web forms, 
+#	-perform CRUD database operations (such as reading data), 
+#	-perform server-side computation
+#	-direct the browser to load certain template HTML/CSS files, inserting dynamic content
+#
+#View functions may have other optional parameters, but always take in a request object parameter containing all 
+#information about the web request, and always produce an HttpResponse object with either redirect() or render()
+#	
+#CONTENTS:
+#1. helper functions
+#2. simple views (that mostly just redirect browser to load either a diff view function or to load a template html/css file)
+#3. showDeliveries: contains most of logic for agentportal
 
 
-#redirects to login page with no credentials	
+
+
+
+
+
+
+
+
+
+#***HELPERFUNCTION***: calcBillForThisMonth
+#DESCRIPTION
+#calculates total cost for all newspaper deliveries for a particular user in a particular month/year. 
+#SIGNATURE
+#(customer customerObject, billDate string, singlePaperPrice float) -> float
+#PSEUDOCODE
+#calcBillForThisMonth(user, singlePaperPrice)
+	#get the list of deliveries for this customer from the 1st to the 20th of the given date's month and year
+	#get length of the list
+	#return length * singlepaperPrice rounded to 2 decimal places
+	
+def calcBillForMonth(customer, billDate, singlePaperPrice):
+	deliveryList = Delivery.objects.filter(customer=customer).filter(date__gte=date(billDate.year, billDate.month-1, 21)).filter(date__lte=date(billDate.year, billDate.month, 20))
+	return round(len(deliveryList)*singlePaperPrice, 2)
+	
+#***HELPERFUNCTION***: generateMapUrl
+#DESCRIPTION
+#creates url containing list of addresses to deliver to formatted to be used by Google Static Images API to generate an image.
+#SIGNATURE
+#(stringList addressList)->string
+def generateMapUrl(addressList):		
+	tailored_url = config.base_url+"&size=320x320&maptype=roadmap&markers=color:blue|"+"".join([address+"|" for address in addressList])+'&key='+config.api_key
+	print(tailored_url)
+	return tailored_url
+
+#***VIEW FUNCTION***: loginLanding
+#DESCRIPTION
+#redirects to login page with no authentication cr	
+#SIGNATURE
+#(httpRequest)->HttpResponse
 def loginLanding(request):
 	return render(request, 'agentportal/login.html')
 
-#takes user credentials from login form
-#if authenticated:
-#	send user to show deliveries page with default settings
+#***VIEW FUNCTION***: loginRedirect
+#DESCRIPTION
+#takes user credentials from login form and either redirects them to showDeliveriesDay view or back to the login page
+#SIGNATURE
+#(httpRequest)->HttpResponse
+#PSEUDOCODE
+#
+#retrieve username and a password from form fields after being submitted
+#use django's built-in user authentication to create a user object 
+#if user was discovered with given username and password
+#	set status to logged in
+#	redirect to showDeliveries-Day view
 #else
-#	send them back to login screen
+#	redirect to login view without setting status to logged in
+
 def loginRedirect(request):
 	username = request.POST['uName']
 	password = request.POST['pWord']
@@ -32,18 +112,63 @@ def loginRedirect(request):
 	else:
 	 	return redirect('/agentportal/')
 
+#***VIEW FUNCTION***: logoutPage
+#DESCRIPTION
 #use django api to log out individual and send them back to login screen
+#SIGNATURE
+#(httpReques)->HttpResponse
 
 def logoutPage(request):
 	logout(request)
 	return redirect('/agentportal/')
 
 
-##MAIN LOGIC FOR DELIVERY VIEWING
-#	contains options for single day or range of dates (mode=day vs mode = range)
-#	when transitioning from login screen, always set mode to day
-#	when submitting form from this page, reloads the same page with mode=range, using values for beginning and end date from form 
-#	
+#***VIEW FUNCTION***: showDeliveries
+#DESCRIPTION
+#This is the main view utilized by our users. The purpose is to generate a page relevant to the logged-in delivery agent 
+#for them to learn information about their current and or past deliveries including when,where, to whom, and whether bills 
+#are due in a given range of dates by providing a list and a map image of each delivery location.
+#ShowDeliveries has 2 modes of operation: 
+#	-(default when transitioning from login page): display delivery information for given user for current day
+#	-take in form data about range of dages and display delivery information for those dates 				
+#
+#SIGNATURE
+#(httpRequest, string mode)->HttpResponse	
+#
+#CONSTRAINTS
+#	-only display delivery information relevant to logged in user(newspaper agent)
+#	-do not generate deliveries for customers that are on vacation
+#	-if range of deliveries includes the 20th of that month, generate a special delivery that includes each customer's
+#		bill using calcBillForMonth helper function..
+#	-display up to 15 of the delivery addresses on a visual map
+#
+#PSEUDOCODE
+#if the user is not authenticated
+#	redirect them to logout page	
+#if mode is set to day
+#	set begDate and endDate variables to the current day
+#else
+#	set begDate and endDate by using POSTed form data
+#if form data is not valid
+#	give a warning and set endDate and begDate to current day
+#convert begDate and endDate variables to datetime.date() objects
+#if the endDate is before begDate
+#	swap the values
+# for all deliveries with given user that are >= begDate and are <=endDate
+#	append a dict to the dateNameAddressList 
+#if the 20th falls between endDate and begDate
+#	for all customers
+#		append a entry to dateNameAddressList including bill calculated with calcBillForMonth helper function
+#if number of items in dateNameAddressList is greater than 15
+#	generate a warning message that Google maps API can only handle 15 markers
+#generate the url for google static maps api by passing information from dateNameAddressList
+#return HttpResponse object using same html file as template, but passing on:
+#	-username
+#	-dateNameAddressList
+#	-google maps url
+#	-delivery message
+#	-current date
+
 def showDeliveries(request, mode): 
 #	if not authenticated, return to login
 	if not request.user.is_authenticated:
@@ -52,6 +177,7 @@ def showDeliveries(request, mode):
 #	list of delivery info that will be passed to template to display deliveries
 	dateNameAddressList=[]
 	todaysDate = timezone.localtime(timezone.now()).date()
+
 #	set mode to either deliver just today's deliveries or between a range. 		
 	if mode=="day":
 		begDate = todaysDate
@@ -71,26 +197,36 @@ def showDeliveries(request, mode):
 		except Exception as e:
 			print("invalid end date submitted in form. setting to today's date.")
 			endDate = todaysDate
-		
+	
+	#the form submission changes the datatype of these variables from datetime.date() objects to 
+	#strings. for code consistency, let's check if they are date() objects, and if not, make them date objects
+	if not isinstance(begDate, date):
+		begDateList = list(map(lambda x: int(x), begDate.split('-')))
+		begDate = date(begDateList[0], begDateList[1], begDateList[2])
+	if not isinstance(endDate, date):
+		endDateList = list(map(lambda x: int(x), endDate.split('-')))
+		endDate = date(endDateList[0], endDateList[1],endDateList[2])
+	
 	# if dates are out of order, swap them. 
 	if endDate < begDate:
 		tempDate = begDate
 		begDate = endDate
-		endDate = tempDate
-	
-	#for customer in Customer.objects.all():	
-	#	if begDate.day < 20 and endDate.day > 20:
-	#		dateNameAddressList.append({'date': date(todaysDate.year, todaysDate.month, 20), 'name': customer.name+"(BILL DUE FOR $)"+str(calcBillForMonth(customer, date.today(), 3.25))})
-	
-#	populate list nameAddressList with {date,name, address} dicts to fill the 	
+		endDate = tempDate	
+		
+#	populate list nameAddressList with {date,name, address} dicts to fill the view 	
 	for delivery in Delivery.objects.filter(user=request.user).filter(date__gte=begDate).filter(date__lte=endDate):
 		custName = delivery.customer.name
-		dateNameAddressList.append({'date': delivery.date, 'name':custName, 'address':delivery.customer.address})
+		dateNameAddressList.append({'date': delivery.date, 'name':custName, 'address':delivery.customer.address})	
+		
+#	add a an entry for the bill if the 20th falls between begDate and endDate for all years and months that are spanned from begDate and endDate
+	for yearNum in range(begDate.year, endDate.year+1):
+		for monthNum in range(begDate.month, endDate.month+1):	
+			billDate = date(yearNum, monthNum, 20)	
+			if begDate <= billDate and endDate >= billDate:
+				for customer in Customer.objects.filter(user=request.user):
+					dateNameAddressList.append({'date': billDate, 'name': "(BILL DUE for "+str(calcBillForMonth(customer, billDate, .87))+"):"+customer.name, 'address':customer.address})	
 	
-	
-#	generate custom image url to generate google static map using address list	
-	tailored_url = generateMapUrl([item['address'] for item in dateNameAddressList])
-	
+
 #	generate delivery message to show above list of deliveries. if there are more than 15 deliveries, 
 #	append message explaining why markers do not show up on google maps image. 
 	deliveryListMessage = ""
@@ -98,27 +234,8 @@ def showDeliveries(request, mode):
 		deliveryListMessage = "(google maps cannot display that many deliveries. limit to 15 or less) "
 	deliveryListMessage +="from %s to %s" % (begDate, endDate)
 	
+#	generate custom image url to generate google static map using address list	
+	tailored_url = generateMapUrl([item['address'] for item in dateNameAddressList])
 	
-	return render(request, 'agentportal/index.html', {'userName':request.user.username, 'dateNameAddressList':dateNameAddressList, 'mapURL':tailored_url, 'todaysDate': str(todaysDate), 'deliveryListMessage': deliveryListMessage })
+	return render(request, 'agentportal/index.html', {'userName':request.user.username, 'dateNameAddressList':dateNameAddressList, 'mapURL':tailored_url, 'todaysDate': str(todaysDate), 'endDate': endDate, 'begDate': endDate, 'deliveryListMessage': deliveryListMessage })
 
-#***DESCRIPTION***
-#calculates total cost for all newspaper deliveries for a particular user in a particular month/year. 
-#
-#***SIGNATURE***
-#(customer customerObject, dateInMonth string, singlePaperPrice float) -> float
-#
-#***PSEUDOCODE***
-#calcBillForThisMonth(user, singlePaperPrice)
-	#get the list of deliveries for this customer from the 1st to the 20th of the given date's month and year
-	#get length of the list
-	#return length * singlepaperPrice rounded to 2 decimal places
-	
-def calcBillForMonth(customer, dateInMonth, singlePaperPrice):
-	deliveryList = Delivery.objects.filter(customer=customer).filter(date__gte=date(dateInMonth.year, dateInMonth.day, 1)).filter(date__lte=date(dateInMonth.year, dateInMonth.month, 20))
-	return round(len(deliveryList)*singlePaperPrice, 2)
-	
-
-def generateMapUrl(addressList):		
-	tailored_url = config.base_url+"&size=320x320&maptype=roadmap&markers=color:blue|"+"".join([address+"|" for address in addressList])+'&key='+config.api_key
-	print(tailored_url)
-	return tailored_url
